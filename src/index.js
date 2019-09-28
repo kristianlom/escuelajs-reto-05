75bb5806e9a7ad515d5fc0e04a2ed119c99358a9
Kristian Lopez <kristian.lom@gmail.com>
2019-09-28 14:22:09 -0500

    Solución Primer Problema

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+const myStorage = window.localStorage;
+myStorage.setItem('next_page', API);
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      myStorage.setItem('next_page', response.info.next);
       let output = characters.map(character => {
         return `
       <article class="Card">
-        <img src="${character.image}" />
+        <img src="${character.image}" alt="${character.name}"/>
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
     `
@@ -21,15 +24,15 @@ const getData = api => {
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
-}
+};
 
-const loadData = () => {
-  getData(API);
-}
+const loadData =  api => {
+    getData(api);
+};
 
 const intersectionObserver = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
-    loadData();
+    loadData(myStorage.getItem('next_page'));
   }
 }, {
   rootMargin: '0px 0px 100% 0px',
